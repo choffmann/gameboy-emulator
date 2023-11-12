3d81df86dd9f2a98c1045338d76c9ed4c5662954
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2023-11-12 22:05:05 +0100

    Finished 8 bit register instructions

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -1,6 +1,6 @@
 use crate::memory::Memory;
 
-use super::instructions::ArithmeticTarget;
+use super::instructions::Logic8BitRegister;
 use super::instructions::Instruction;
 use super::registers::Registers;
 
@@ -18,53 +18,160 @@ impl CPU {
             Instruction::SUB(target) => self.match_sub(target, false),
             Instruction::SBC(target) => self.match_sub(target, true),
             Instruction::INC(target) => self.match_inc(target),
-            _ => panic!("Not implemented instructions {:?}", instructions),
+            Instruction::DEC(target) => self.match_dec(target),
+            Instruction::AND(target) => self.match_and(target),
+            Instruction::OR(target) => self.match_or(target),
+            Instruction::XOR(target) => self.match_xor(target),
+            Instruction::CP(target) => self.match_cp(target),
         }
     }
 
-    fn match_add(&mut self, target: ArithmeticTarget, with_carry: bool) {
+    fn match_add(&mut self, target: Logic8BitRegister, with_carry: bool) {
         match target {
-            ArithmeticTarget::B => {
-                let value = self.register.b;
-                let new_value = self.add(value, with_carry);
-                self.register.a = new_value;
-            }
-            _ => println!(
-                "Missing regsiter on ADD Instruction, target register: {:?}",
-                target
-            ),
-        };
-    }
-
-    fn match_inc(&mut self, target: ArithmeticTarget) {
-        match target {
-            ArithmeticTarget::B => {
-                let value = self.register.b;
-                self.register.b = self.inc(value);
-            }
-            _ => println!(
-                "Missing regsiter on INC Instruction, target register: {:?}",
-                target
-            ),
+            Logic8BitRegister::A => self.exec_add(self.register.a, with_carry),
+            Logic8BitRegister::B => self.exec_add(self.register.b, with_carry),
+            Logic8BitRegister::C => self.exec_add(self.register.c, with_carry),
+            Logic8BitRegister::D => self.exec_add(self.register.d, with_carry),
+            Logic8BitRegister::E => self.exec_add(self.register.e, with_carry),
+            Logic8BitRegister::H => self.exec_add(self.register.h, with_carry),
+            Logic8BitRegister::L => self.exec_add(self.register.l, with_carry),
+            Logic8BitRegister::D8 => todo!("Not implemented"),
+            Logic8BitRegister::HLI => todo!("Not implemented"),
         }
     }
 
-    fn match_sub(&mut self, target: ArithmeticTarget, with_carry: bool) {
+    fn match_inc(&mut self, target: Logic8BitRegister) {
         match target {
-            ArithmeticTarget::B => {
-                let value = self.register.b;
-                let new_value = self.sub(value, with_carry);
-                self.register.a = new_value;
-            }
-            _ => println!(
-                "Missing regsiter on SUB Instruction, target register: {:?}",
-                target
-            ),
+            Logic8BitRegister::A => self.register.a = self.inc(self.register.a),
+            Logic8BitRegister::B => self.register.b = self.inc(self.register.b),
+            Logic8BitRegister::C => self.register.c = self.inc(self.register.c),
+            Logic8BitRegister::D => self.register.d = self.inc(self.register.d),
+            Logic8BitRegister::E => self.register.e = self.inc(self.register.e),
+            Logic8BitRegister::H => self.register.h = self.inc(self.register.h),
+            Logic8BitRegister::L => self.register.l = self.inc(self.register.l),
+            Logic8BitRegister::D8 => todo!("Not implemented"),
+            Logic8BitRegister::HLI => todo!("Not implemented")
+        }
+    }
+
+    fn match_dec(&mut self, target: Logic8BitRegister) {
+        match target {
+            Logic8BitRegister::A => self.register.a = self.dec(self.register.a),
+            Logic8BitRegister::B => self.register.b = self.dec(self.register.b),
+            Logic8BitRegister::C => self.register.c = self.dec(self.register.c),
+            Logic8BitRegister::D => self.register.d = self.dec(self.register.d),
+            Logic8BitRegister::E => self.register.e = self.dec(self.register.e),
+            Logic8BitRegister::H => self.register.h = self.dec(self.register.h),
+            Logic8BitRegister::L => self.register.l = self.dec(self.register.l),
+            Logic8BitRegister::D8 => todo!("Not implemented"),
+            Logic8BitRegister::HLI => todo!("Not implemented")
+        }
+    }
+
+    fn match_sub(&mut self, target: Logic8BitRegister, with_carry: bool) {
+        match target {
+            Logic8BitRegister::A => self.exec_sub(self.register.a, with_carry),
+            Logic8BitRegister::B => self.exec_sub(self.register.b, with_carry),
+            Logic8BitRegister::C => self.exec_sub(self.register.c, with_carry),
+            Logic8BitRegister::D => self.exec_sub(self.register.d, with_carry),
+            Logic8BitRegister::E => self.exec_sub(self.register.e, with_carry),
+            Logic8BitRegister::H => self.exec_sub(self.register.h, with_carry),
+            Logic8BitRegister::L => self.exec_sub(self.register.l, with_carry),
+            Logic8BitRegister::D8 => todo!("Not implemented"),
+            Logic8BitRegister::HLI => todo!("Not implemented")
+        }
+    }
+
+    fn match_and(&mut self, target: Logic8BitRegister) {
+        match target {
+            Logic8BitRegister::A => self.register.a = self.and(self.register.a),
+            Logic8BitRegister::B => self.register.b = self.and(self.register.b),
+            Logic8BitRegister::C => self.register.c = self.and(self.register.c),
+            Logic8BitRegister::D => self.register.d = self.and(self.register.d),
+            Logic8BitRegister::E => self.register.e = self.and(self.register.e),
+            Logic8BitRegister::H => self.register.h = self.and(self.register.h),
+            Logic8BitRegister::L => self.register.l = self.and(self.register.l),
+            Logic8BitRegister::D8 => todo!("Not implemented"),
+            Logic8BitRegister::HLI => todo!("Not implemented")
+        }
+    }
+
+    fn match_or(&mut self, target: Logic8BitRegister) {
+        match target {
+            Logic8BitRegister::A => self.register.a = self.or(self.register.a),
+            Logic8BitRegister::B => self.register.b = self.or(self.register.b),
+            Logic8BitRegister::C => self.register.c = self.or(self.register.c),
+            Logic8BitRegister::D => self.register.d = self.or(self.register.d),
+            Logic8BitRegister::E => self.register.e = self.or(self.register.e),
+            Logic8BitRegister::H => self.register.h = self.or(self.register.h),
+            Logic8BitRegister::L => self.register.l = self.or(self.register.l),
+            Logic8BitRegister::D8 => todo!("Not implemented"),
+            Logic8BitRegister::HLI => todo!("Not implemented")
+        }
+    }
+
+    fn match_xor(&mut self, target: Logic8BitRegister) {
+        match target {
+            Logic8BitRegister::A => self.register.a = self.xor(self.register.a),
+            Logic8BitRegister::B => self.register.b = self.xor(self.register.b),
+            Logic8BitRegister::C => self.register.c = self.xor(self.register.c),
+            Logic8BitRegister::D => self.register.d = self.xor(self.register.d),
+            Logic8BitRegister::E => self.register.e = self.xor(self.register.e),
+            Logic8BitRegister::H => self.register.h = self.xor(self.register.h),
+            Logic8BitRegister::L => self.register.l = self.xor(self.register.l),
+            Logic8BitRegister::D8 => todo!("Not implemented"),
+            Logic8BitRegister::HLI => todo!("Not implemented")
+        }
+    }
+
+    fn match_cp(&mut self, target: Logic8BitRegister) {
+        match target {
+            Logic8BitRegister::A => self.exec_compare(self.register.a),
+            Logic8BitRegister::B => self.exec_compare(self.register.b),
+            Logic8BitRegister::C => self.exec_compare(self.register.c),
+            Logic8BitRegister::D => self.exec_compare(self.register.d),
+            Logic8BitRegister::E => self.exec_compare(self.register.e),
+            Logic8BitRegister::H => self.exec_compare(self.register.h),
+            Logic8BitRegister::L => self.exec_compare(self.register.l),
+            Logic8BitRegister::D8 => {}
+            Logic8BitRegister::HLI => {}
         }
     }
 
     fn step(&mut self) {
         let mut instruction_byte = self.memory.read_byte(self.pc);
+    }
+
+    fn exec_add(&mut self, value_from_register: u8, with_carry: bool) {
+        self.register.a = self.add(value_from_register, with_carry);
+    }
+
+    fn exec_sub(&mut self, value_from_register: u8, with_carry: bool) {
+        self.register.a = self.sub(value_from_register, with_carry);
+    }
+
+    fn exec_inc(&mut self, value_from_register: u8) {
+        self.register.a = self.inc(value_from_register);
+    }
+
+    fn exec_dec(&mut self, value_from_register: u8) {
+        self.register.a = self.dec(value_from_register);
+    }
+
+    fn exec_and(&mut self, value_from_register: u8) {
+        self.register.a = self.and(value_from_register);
+    }
+
+    fn exec_or(&mut self, value_from_register: u8) {
+        self.register.a = self.or(value_from_register);
+    }
+
+    fn exec_xor(&mut self, value_from_register: u8) {
+        self.register.a = self.xor(value_from_register);
+    }
+
+    fn exec_compare(&mut self, value_from_register: u8) {
+       self.compare(value_from_register);
     }
 
     fn add(&mut self, value: u8, add_carry: bool) -> u8 {
@@ -80,21 +187,63 @@ impl CPU {
 
     fn sub(&mut self, value: u8, add_carry: bool) -> u8 {
         let carry_value = self.get_opt_carry_flag(add_carry);
-        let (sub, firt_did_overflow) = self.register.a.overflowing_sub(value);
+        let (sub, first_did_overflow) = self.register.a.overflowing_sub(value);
         let (new_value, result_did_overflow) = sub.overflowing_sub(sub);
         self.register.f.zero = new_value == 0;
         self.register.f.substract = true;
-        self.register.f.carry = firt_did_overflow || result_did_overflow;
+        self.register.f.carry = first_did_overflow || result_did_overflow;
         self.register.f.half_carry = (self.register.a & 0xf) < (value & 0xf) + carry_value;
         return new_value;
     }
 
     fn inc(&mut self, value: u8) -> u8 {
-        let (new_value, did_overflow) = value.overflowing_add(1);
+        let new_value = value.wrapping_add(1);
         self.register.f.zero = new_value == 0;
         self.register.f.substract = false;
         self.register.f.half_carry = value & 0xf == 0xf;
         return new_value;
+    }
+
+    fn dec(&mut self, value: u8) -> u8 {
+        let new_value = value.wrapping_sub(1);
+        self.register.f.zero = new_value == 0;
+        self.register.f.substract = true;
+        self.register.f.half_carry = value & 0xf == 0x0;
+        return new_value;
+    }
+
+    fn and(&mut self, value: u8) -> u8 {
+        let new_value = self.register.a & value;
+        self.register.f.zero = new_value == 0;
+        self.register.f.substract = false;
+        self.register.f.half_carry = true;
+        self.register.f.carry = false;
+        return new_value;
+    }
+
+    fn or(&mut self, value: u8) -> u8 {
+        let new_value = self.register.a | value;
+        self.register.f.zero = new_value == 0;
+        self.register.f.substract = false;
+        self.register.f.carry = false;
+        self.register.f.half_carry = false;
+        return new_value;
+    }
+
+    fn xor(&mut self, value: u8) -> u8 {
+        let new_value = self.register.a ^ value;
+        self.register.f.zero = new_value == 0;
+        self.register.f.substract = false;
+        self.register.f.carry = false;
+        self.register.f.half_carry = false;
+        return new_value;
+    }
+
+    fn compare(&mut self, value: u8) {
+        self.register.f.zero = self.register.a == value;
+        self.register.f.substract = true;
+        self.register.f.half_carry = (self.register.a & 0xf) < (value & 0xf);
+        self.register.f.carry = self.register.a < value;
     }
 
     fn get_opt_carry_flag(&self, add_carry: bool) -> u8 {
@@ -104,4 +253,8 @@ impl CPU {
             0
         }
     }
+}
+
+fn print_missing_register(instruction: &str, target: Logic8BitRegister) {
+    unimplemented!("Missing instruction {} for register {:?}", instruction, target);
 }
--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -1,15 +1,21 @@
+
 #[derive(Debug)]
 pub enum Instruction {
-    // Arithemtic Instructions
-    ADD(ArithmeticTarget),
-    ADC(ArithmeticTarget),
-    SUB(ArithmeticTarget),
-    SBC(ArithmeticTarget),
-    INC(ArithmeticTarget),
+    // Arithmetic Instructions
+    ADD(Logic8BitRegister),
+    ADC(Logic8BitRegister),
+    SUB(Logic8BitRegister),
+    SBC(Logic8BitRegister),
+    INC(Logic8BitRegister),
+    DEC(Logic8BitRegister),
+    AND(Logic8BitRegister),
+    OR(Logic8BitRegister),
+    XOR(Logic8BitRegister),
+    CP(Logic8BitRegister)
 }
 
 #[derive(Debug)]
-pub enum ArithmeticTarget {
+pub enum Logic8BitRegister {
     A,
     B,
     C,
@@ -21,50 +27,104 @@ pub enum ArithmeticTarget {
     HLI,
 }
 
+#[derive(Debug)]
+pub enum Logic16BitRegister {
+    BC, DE, HL, SP
+}
+
 impl Instruction {
     fn from_byte(byte: u8) -> Option<Instruction> {
         match byte {
-            0x3c => Some(Instruction::INC(ArithmeticTarget::A)),
-            0x04 => Some(Instruction::INC(ArithmeticTarget::B)),
-            0x0c => Some(Instruction::INC(ArithmeticTarget::C)),
-            0x14 => Some(Instruction::INC(ArithmeticTarget::D)),
-            0x1c => Some(Instruction::INC(ArithmeticTarget::E)),
-            0x24 => Some(Instruction::INC(ArithmeticTarget::H)),
-            0x2c => Some(Instruction::INC(ArithmeticTarget::L)),
+            0x3c => Some(Instruction::INC(Logic8BitRegister::A)),
+            0x04 => Some(Instruction::INC(Logic8BitRegister::B)),
+            0x0c => Some(Instruction::INC(Logic8BitRegister::C)),
+            0x14 => Some(Instruction::INC(Logic8BitRegister::D)),
+            0x1c => Some(Instruction::INC(Logic8BitRegister::E)),
+            0x24 => Some(Instruction::INC(Logic8BitRegister::H)),
+            0x2c => Some(Instruction::INC(Logic8BitRegister::L)),
+            0x34 => Some(Instruction::INC(Logic8BitRegister::HLI)),
 
-            0x87 => Some(Instruction::ADD(ArithmeticTarget::A)),
-            0x80 => Some(Instruction::ADD(ArithmeticTarget::B)),
-            0x81 => Some(Instruction::ADD(ArithmeticTarget::C)),
-            0x82 => Some(Instruction::ADD(ArithmeticTarget::D)),
-            0x83 => Some(Instruction::ADD(ArithmeticTarget::E)),
-            0x84 => Some(Instruction::ADD(ArithmeticTarget::H)),
-            0x85 => Some(Instruction::ADD(ArithmeticTarget::L)),
-            0x86 => Some(Instruction::ADD(ArithmeticTarget::HLI)),
-            0xc6 => Some(Instruction::ADD(ArithmeticTarget::D8)),
+            0x87 => Some(Instruction::ADD(Logic8BitRegister::A)),
+            0x80 => Some(Instruction::ADD(Logic8BitRegister::B)),
+            0x81 => Some(Instruction::ADD(Logic8BitRegister::C)),
+            0x82 => Some(Instruction::ADD(Logic8BitRegister::D)),
+            0x83 => Some(Instruction::ADD(Logic8BitRegister::E)),
+            0x84 => Some(Instruction::ADD(Logic8BitRegister::H)),
+            0x85 => Some(Instruction::ADD(Logic8BitRegister::L)),
+            0x86 => Some(Instruction::ADD(Logic8BitRegister::HLI)),
+            0xc6 => Some(Instruction::ADD(Logic8BitRegister::D8)),
 
-            0x8f => Some(Instruction::ADC(ArithmeticTarget::A)),
-            0x88 => Some(Instruction::ADC(ArithmeticTarget::B)),
-            0x89 => Some(Instruction::ADC(ArithmeticTarget::C)),
-            0x8A => Some(Instruction::ADC(ArithmeticTarget::D)),
-            0x8B => Some(Instruction::ADC(ArithmeticTarget::E)),
-            0x8C => Some(Instruction::ADC(ArithmeticTarget::H)),
-            0x8D => Some(Instruction::ADC(ArithmeticTarget::L)),
+            0x8f => Some(Instruction::ADC(Logic8BitRegister::A)),
+            0x88 => Some(Instruction::ADC(Logic8BitRegister::B)),
+            0x89 => Some(Instruction::ADC(Logic8BitRegister::C)),
+            0x8A => Some(Instruction::ADC(Logic8BitRegister::D)),
+            0x8B => Some(Instruction::ADC(Logic8BitRegister::E)),
+            0x8C => Some(Instruction::ADC(Logic8BitRegister::H)),
+            0x8D => Some(Instruction::ADC(Logic8BitRegister::L)),
 
-            0x97 => Some(Instruction::SUB(ArithmeticTarget::A)),
-            0x90 => Some(Instruction::SUB(ArithmeticTarget::B)),
-            0x91 => Some(Instruction::SUB(ArithmeticTarget::C)),
-            0x92 => Some(Instruction::SUB(ArithmeticTarget::D)),
-            0x93 => Some(Instruction::SUB(ArithmeticTarget::E)),
-            0x94 => Some(Instruction::SUB(ArithmeticTarget::H)),
-            0x95 => Some(Instruction::SUB(ArithmeticTarget::L)),
+            0x97 => Some(Instruction::SUB(Logic8BitRegister::A)),
+            0x90 => Some(Instruction::SUB(Logic8BitRegister::B)),
+            0x91 => Some(Instruction::SUB(Logic8BitRegister::C)),
+            0x92 => Some(Instruction::SUB(Logic8BitRegister::D)),
+            0x93 => Some(Instruction::SUB(Logic8BitRegister::E)),
+            0x94 => Some(Instruction::SUB(Logic8BitRegister::H)),
+            0x95 => Some(Instruction::SUB(Logic8BitRegister::L)),
 
-            0x9f => Some(Instruction::SBC(ArithmeticTarget::A)),
-            0x98 => Some(Instruction::SBC(ArithmeticTarget::B)),
-            0x99 => Some(Instruction::SBC(ArithmeticTarget::C)),
-            0x9a => Some(Instruction::SBC(ArithmeticTarget::D)),
-            0x9b => Some(Instruction::SBC(ArithmeticTarget::E)),
-            0x9c => Some(Instruction::SBC(ArithmeticTarget::H)),
-            0x9d => Some(Instruction::SBC(ArithmeticTarget::L)),
+            0x9f => Some(Instruction::SBC(Logic8BitRegister::A)),
+            0x98 => Some(Instruction::SBC(Logic8BitRegister::B)),
+            0x99 => Some(Instruction::SBC(Logic8BitRegister::C)),
+            0x9a => Some(Instruction::SBC(Logic8BitRegister::D)),
+            0x9b => Some(Instruction::SBC(Logic8BitRegister::E)),
+            0x9c => Some(Instruction::SBC(Logic8BitRegister::H)),
+            0x9d => Some(Instruction::SBC(Logic8BitRegister::L)),
+
+            0xa7 => Some(Instruction::AND(Logic8BitRegister::A)),
+            0xa0 => Some(Instruction::AND(Logic8BitRegister::B)),
+            0xa1 => Some(Instruction::AND(Logic8BitRegister::C)),
+            0xa2 => Some(Instruction::AND(Logic8BitRegister::D)),
+            0xa3 => Some(Instruction::AND(Logic8BitRegister::E)),
+            0xa4 => Some(Instruction::AND(Logic8BitRegister::H)),
+            0xa6 => Some(Instruction::AND(Logic8BitRegister::HLI)),
+            0xe6 => Some(Instruction::AND(Logic8BitRegister::D8)),
+
+            0xb7 => Some(Instruction::OR(Logic8BitRegister::A)),
+            0xb0 => Some(Instruction::OR(Logic8BitRegister::B)),
+            0xb1 => Some(Instruction::OR(Logic8BitRegister::C)),
+            0xb2 => Some(Instruction::OR(Logic8BitRegister::D)),
+            0xb3 => Some(Instruction::OR(Logic8BitRegister::E)),
+            0xb4 => Some(Instruction::OR(Logic8BitRegister::H)),
+            0xb5 => Some(Instruction::OR(Logic8BitRegister::L)),
+            0xb6 => Some(Instruction::OR(Logic8BitRegister::HLI)),
+            0xf6 => Some(Instruction::OR(Logic8BitRegister::D8)),
+
+            0xaf => Some(Instruction::XOR(Logic8BitRegister::A)),
+            0xa8 => Some(Instruction::XOR(Logic8BitRegister::B)),
+            0xa9 => Some(Instruction::XOR(Logic8BitRegister::C)),
+            0xaa => Some(Instruction::XOR(Logic8BitRegister::D)),
+            0xab => Some(Instruction::XOR(Logic8BitRegister::E)),
+            0xac => Some(Instruction::XOR(Logic8BitRegister::H)),
+            0xad => Some(Instruction::XOR(Logic8BitRegister::L)),
+            0xae => Some(Instruction::XOR(Logic8BitRegister::HLI)),
+            0xee => Some(Instruction::XOR(Logic8BitRegister::D8)),
+
+            0xbf => Some(Instruction::CP(Logic8BitRegister::A)),
+            0xb8 => Some(Instruction::CP(Logic8BitRegister::B)),
+            0xb9 => Some(Instruction::CP(Logic8BitRegister::C)),
+            0xba => Some(Instruction::CP(Logic8BitRegister::D)),
+            0xbb => Some(Instruction::CP(Logic8BitRegister::E)),
+            0xbc => Some(Instruction::CP(Logic8BitRegister::H)),
+            0xbd => Some(Instruction::CP(Logic8BitRegister::L)),
+            0xbe => Some(Instruction::CP(Logic8BitRegister::HLI)),
+            0xfe => Some(Instruction::CP(Logic8BitRegister::D8)),
+
+            0x3d => Some(Instruction::DEC(Logic8BitRegister::A)),
+            0x05 => Some(Instruction::DEC(Logic8BitRegister::B)),
+            0x0d => Some(Instruction::DEC(Logic8BitRegister::C)),
+            0x15 => Some(Instruction::DEC(Logic8BitRegister::D)),
+            0x1d => Some(Instruction::DEC(Logic8BitRegister::E)),
+            0x25 => Some(Instruction::DEC(Logic8BitRegister::H)),
+            0x2d => Some(Instruction::DEC(Logic8BitRegister::L)),
+            0x35 => Some(Instruction::DEC(Logic8BitRegister::HLI)),
 
             _ => {
                 println!("Missing byte Instruction 0x{:x}", byte);
