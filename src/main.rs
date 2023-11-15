0c20bb70849cde87ed0f721f0182218a41b5544c
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2023-11-15 16:09:29 +0100

    Read bin rom file

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,17 @@
-use std::num::ParseIntError;
+use std::io::Read;
 use crate::cpu::cpu::CPU;
 
 mod cpu;
 mod memory;
 
 fn main() {
-    let boot_rom = read_boot_rom();
+    let boot_rom = load_boot_rom();
     let mut cpu = CPU::boot(boot_rom);
     loop {
         cpu.step();
     }
 }
 
-fn read_boot_rom() -> Vec<u8> {
-    let file = std::fs::read_to_string("./rom/dmg_boot.txt").expect("Can't read file");
-    return decode_hex(&file).unwrap();
-}
-
-fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
-    (0..s.len())
-        .step_by(2)
-        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
-        .collect()
+fn load_boot_rom() -> Vec<u8> {
+    return std::fs::read("./rom/dmg_boot.bin").expect("Can't read file");
 }
